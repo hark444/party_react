d985570a13a2ed4ea474855f1d66fe9199678243
Harshad Keshwani <hark44>
2022-10-09 16:59:37 +0530

    Added React routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import SignupForm from './components/SignupForm';
 import Header from './components/Header';
+import SignupForm from './Pages/SignupForm';
 import './App.css';
+import { Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div>
       <Header />
-      <SignupForm />
+      <Switch>
+        <Route path="/sign-up">
+          <SignupForm />
+        </Route>
+        <Route path="login">
+          <div>
+            <h2>Login Page</h2>
+          </div>
+        </Route>
+      </Switch>
     </div>
   );
 }
