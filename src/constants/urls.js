ef6f4cb6a04bdbb8caa241fe6d74e4bee06f95f3
Harshad Keshwani <hark44>
2022-10-28 18:31:20 +0530

    Integrated user page with define api

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,2 +1,3 @@
 export const AUTH_TOKEN_GENERATE = "http://127.0.0.1:8002/api/v1/auth/token"
 export const USER_CREATE = "http://127.0.0.1:8002/api/v1/users/create"
+export const DEFINE_ME = "http://127.0.0.1:8002/api/v1/auth/define-me"
