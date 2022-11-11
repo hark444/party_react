5b4107b2374621ec1f8dcb8733a5e716fbaa8043
Harshad Keshwani <hark44>
2022-11-11 17:42:50 +0530

    Added auth locks for all pages

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -2,15 +2,16 @@ import React from "react";
 import "../style.css"
 import AuthContext from "../Auth/authContext";
 
+
 export default function HomePage() {
 
-    const Authctx = React.useContext(AuthContext);
+    const authCtx = React.useContext(AuthContext)
 
     return (
         <div className="container">
             <h1>Home Page</h1>
-            <p>{Authctx.username}</p>
-            <p>{Authctx.isLoggedIn.toString()}</p>
+            <p>{authCtx.username}</p>
+            <p>{authCtx.isLoggedIn.toString()}</p>
         </div>
     )
 }
